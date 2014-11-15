2f15f309a8277b73ee9e45983f46167d44309a6c
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-11-15 13:02:44 +0100

    Updated the navigation blade to fit project needs

--- app/routes.php
+++ app/routes.php
@@ -1,17 +1,33 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
-
 Route::get('/', function()
 {
 	return View::make('programme');
+});
+
+Route::get('/programme', function()
+{
+	return Redirect::to('/');
+});
+
+
+Route::get('/manage/events', function()
+{
+	return View::make('manage.events');
+});
+
+Route::get('/manage/price-groups', function()
+{
+	return View::make('manage.price-groups');
+});
+
+Route::get('/manage/genres', function()
+{
+	return View::make('manage.genres');
+});
+
+
+Route::get('/about', function()
+{
+	return View::make('about');
 });
