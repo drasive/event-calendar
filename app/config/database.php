3645c5cac89886e03b5a18767d1678d30f2c4bb3
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-11-15 21:30:13 +0100

    Configured the MySQL database access

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'eventcalendar',
+			'username'  => 'ec-webserver',
+			'password'  => 'YNFKm8y3TjFQSCyDev6F9RF2NkZ8N5Ft',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
