bdf9f81012b53fb70e57e77905db2868af100cbb
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-11-17 10:59:48 +0100

    Added scripts to insert/ delete test data

---
+++ app/database/delete-all-data.sql
@@ -0,0 +1,7 @@
+DELETE FROM `eventcalendar`.`shows`;
+DELETE FROM `eventcalendar`.`links`;
+DELETE FROM `eventcalendar`.`event_pricegroups`;
+DELETE FROM `eventcalendar`.`pricegroups`;
+DELETE FROM `eventcalendar`.`events`;
+DELETE FROM `eventcalendar`.`genres`;
+DELETE FROM `eventcalendar`.`users`;
