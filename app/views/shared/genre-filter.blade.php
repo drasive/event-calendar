26d20a3d2078035ff76b32a0d66ce4001b8d907f
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-12-15 13:21:54 +0100

    Fixed the bug with "All Genres" redirecting from the Archive to the Programme page

--- app/views/shared/genre-filter.blade.php
+++ app/views/shared/genre-filter.blade.php
@@ -1,7 +1,7 @@
 @if (count($genres) > 1)
     <div class="genre-filter btn-group"  role="group">
-        <a href="." class="btn btn-default
-          @if (is_null($selectedGenre)) active @endif">All Genres</a>
+        <a href="?page=1" class="btn btn-default @if (is_null($selectedGenre)) active @endif"
+          >All Genres</a>
         
         @foreach ($genres as $index => $genre)
             <a class="http-parameter btn btn-default
